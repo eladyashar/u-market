b5b99e3fe01762bed555e082befe4f1bb9291819
ramigoldvarg <ramigoldvarg7@walla.co.il>
2021-04-12 22:31:58 +0300

    Initial DB setting

---
+++ u-market/DAL/MarketContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace u_market.DAL
+{
+    public class MarketContext : DbContext
+    {
+        public MarketContext(DbContextOptions<MarketContext> options) : base(options)
+        {
+
+        }
+    }
+}
