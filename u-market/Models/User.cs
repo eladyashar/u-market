d6d9159852a94f856669551e9c54605b587a0658
ramigoldvarg <ramigoldvarg7@walla.co.il>
2021-06-02 17:45:27 +0300

    user change

---
+++ u-market/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace u_market.Models
+{
+    public enum UserType
+    {
+        Client,
+        Admin
+    }
+
+    public class User
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("username")]
+        public string Username { get; set; }
+        [Column("password")]
+        public string Password { get; set; }
+        [Column("type")]
+        public UserType Type { get; set; } = UserType.Client;
+    }
+}
