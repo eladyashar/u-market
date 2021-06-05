455fc9fe4c3e73d7d250514da59ec76032fd11f5
Uriel Wasyng <urielwasyng@gmail.com>
2021-06-05 18:49:39 +0300

    wip

---
+++ u-market/Models/Purchase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace u_market.Models
+{
+    public class Purchase
+    {
+        [Key, Column(Order = 0)]
+        [ForeignKey("username")]
+        public string Username { get; set; }
+        [ForeignKey("product_id")]
+        [Key, Column(Order = 1)]
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual User User { get; set; }
+        [Column("time_stamp")]
+        public DateTime PurchaseDate { get; set; }
+
+        public Purchase() {
+            this.PurchaseDate = DateTime.UtcNow;
+        }
+    }
+}
