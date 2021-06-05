44708a1183990f71b46dd740f800826292906b1d
yoniKliger <yonikliger@gmail.com>
2021-06-05 14:26:46 +0300

    Feat/users/add user management view (#10)
    * wip
    
    * added server side of UsersManagement
    
    * wip

---
+++ u-market/Controllers/Users/UsersManagementController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using u_market.DAL;
+
+namespace u_market.Controllers.Users
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersManagementController : Controller
+    {
+        private UsersManagementLogic Logic { get; }
+
+        public UsersManagementController(MarketContext ctx)
+        {
+            Logic = new UsersManagementLogic(ctx);
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Users = Logic.GetAll();
+            return View();
+
+        }
+    }
+}
