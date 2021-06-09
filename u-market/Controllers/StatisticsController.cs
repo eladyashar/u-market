a5ed5b986713c643018fc84205cbc00f0bd98a73
Uriel Wasyng <urielwasyng@gmail.com>
2021-06-09 23:42:31 +0300

    start of statistics wip

---
+++ u-market/Controllers/StatisticsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using u_market.Models;
+using u_market.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace u_market.Controllers
+{
+    [Authorize(Roles = "Admin,Client")]
+    public class StatisticsController : Controller
+    {
+        private readonly MarketContext Ctx;
+        public StatisticsController(MarketContext Ctx)
+        {
+            this.Ctx = Ctx;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.bla = "me";
+            return View();
+        }
+    }
+}
