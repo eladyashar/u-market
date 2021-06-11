0d8b3bb98c76fcf385187f058a70d528ebcd14e1
ramigoldvarg <ramigoldvarg7@walla.co.il>
2021-06-11 18:02:16 +0300

    Small project cleanup

--- u-market/Controllers/Prducts/ProductController.cs
+++ u-market/Controllers/Prducts/ProductController.cs
@@ -31,25 +31,5 @@ namespace u_market.Controllers
 
             return StatusCode(200);
         }
-
-        public IActionResult Index()
-        {
-            ViewBag.NumTimes = GetAll().Select(c => c.Name).ToList()[0];
-            return View();
-        }
-
-        private List<Product> GetAll()
-        {
-            return Ctx.Products.ToList();
-        }
-
-        public ActionResult Welcome(string name, int numTimes = 1)
-        {
-            ViewBag.Message = "Hello " + name;
-            ViewBag.NumTimes = numTimes;
-
-            return View();
-        }
-
     }
 }
