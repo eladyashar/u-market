c8fc7e533f86be866e6660312e4f6aab4d2a71a8
ramigoldvarg <ramigoldvarg7@walla.co.il>
2021-06-12 20:18:21 +0300

    Tag management works

--- u-market/DAL/MarketContext.cs
+++ u-market/DAL/MarketContext.cs
@@ -23,9 +23,10 @@ namespace u_market.DAL
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>().ToTable("products").Property(p => p.Id).ValueGeneratedOnAdd();
-            modelBuilder.Entity<Store>().ToTable("stores");
+            modelBuilder.Entity<Store>().ToTable("stores").Property(s => s.Id).ValueGeneratedOnAdd();
             modelBuilder.Entity<User>().ToTable("users");
             modelBuilder.Entity<Purchase>().ToTable("purchases").HasKey(c => new { c.Username, c.ProductId, c.PurchaseDate });
+            modelBuilder.Entity<Tag>().ToTable("tags").Property(t => t.Id).ValueGeneratedOnAdd();
             modelBuilder.Entity<Tag>().ToTable("tags").HasMany<Product>(t => t.Products).WithMany(p => p.Tags)
                 .UsingEntity<Dictionary<string, object>>(
                 "product_tags",
---
+++ u-market/wwwroot/js/tag.js
@@ -0,0 +1,76 @@
+﻿let allTags = []
+const loadAllTags = () =>
+    $.ajax({
+        url: '/Tag/GetAll/',
+        type: 'GET',
+        success: (data) => {
+            allTags = data;
+        },
+        error: () => {
+            alert('an error occured');
+        }
+    });
+
+const generateTagsTable = () => {
+    const tableBodyElement = $('#tagsTableBody');
+    tableBodyElement.empty();
+    loadAllTags().then(() => {
+        allTags.forEach((tag) => {
+            const row = tableBodyElement.append('<tr>').children('tr:last');
+            row.append(`<td>${tag.id}</td>`)
+                .append(`<td>${tag.name}</td>`)
+
+            const editCol = row.append('<td>').children('td:last');
+            editCol.append(`<i class='fa fa-pen' onclick="openEditModal({tagName:'${tag.name}', tagId: ${tag.id}})"></i>`);
+
+            const deleteCol = row.append('<td>').children('td:last');
+            deleteCol.append(`<i class='fa fa-trash' onclick="removeTag('${tag.id}')"></i>`)
+        })
+    });
+};
+
+const openEditModal = tag => {
+    const editModal = $('#editTagModal');
+
+    editModal.find('#tagId').attr('placeholder', tag.tagId).val(tag.tagId);
+    editModal.find('#tagName').attr('placeholder', tag.tagName).val(tag.tagName);
+    editModal.find('#submit').attr('onclick', `saveTag()`)
+
+    editModal.modal('show');
+}
+
+const saveTag = () => {
+
+    const [newTagName, tagId] = [$('#editTagModal #tagName').val(), $('#editTagModal #tagId').val()];
+
+    $.ajax({
+        url: '/Tag/UPDATE/',
+        type: 'PUT',
+        data: JSON.stringify({ id: parseInt(tagId), name: `${newTagName}`}),
+        contentType: "application/json; charset=utf-8",
+        success: () => {
+            $('#editTagModal').modal('hide');
+            generateTagsTable();
+        },
+        error: () => {
+            alert('an error occured');
+        }
+    });
+};
+
+const removeTag = tagId => {
+    $.ajax({
+        url: `/Tag/Delete?id=${tagId}`,
+        type: 'DELETE',
+        success: () => {
+            generateTagsTable();
+        },
+        error: () => {
+            alert('an error occured');
+        }
+    });
+};
+
+$(document).ready(() => {
+    generateTagsTable();
+});
