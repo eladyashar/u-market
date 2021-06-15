33e3fd19b8b801537896b746120db5dfdad4f3c1
Yoni Kliger <yonikliger@gmail.com>
2021-06-16 00:01:07 +0300

    added address autocomplete with google maps API

---
+++ u-market/wwwroot/js/store.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(() => {
+    $('.address-input').toArray().forEach(addressInput => {
+        new google.maps.places.Autocomplete(addressInput);
+    });
+});
+
+getCodedAddress = () => {
+    var geocoder = new google.maps.Geocoder();
+    var address = $('.address-input')[0].value;
+    geocoder.geocode({address}, (results, status) => {
+        if (status == 'OK') {
+            const location = results[0].geometry.location;
+            console.log(`lat: ${location.lat()}, long: ${location.lng()}`)
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
