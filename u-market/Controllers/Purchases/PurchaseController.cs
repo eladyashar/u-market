9f7111313f5188d567f2c5aca184e07788a6817f
Elad Yashar <eladyashar1104@gmail.com>
2021-06-19 16:41:37 +0300

    add purchase history table without filter

---
+++ u-market/Controllers/Purchases/PurchaseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using u_market.DAL;
+using Microsoft.EntityFrameworkCore;
+using u_market.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace u_market.Controllers.Purchases
+{
+    public class PurchaseController : Controller
+    {
+        private readonly MarketContext Ctx;
+        public PurchaseController(MarketContext Ctx)
+        {
+            this.Ctx = Ctx;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Purchases = getAll();
+            return View();
+        }
+        public IList<Purchase> getAll()
+        {
+            return Ctx.Purchases.Include(c=>c.Product).Include(p=> p.User).ToList();
+        }
+    }
+}
+ 
