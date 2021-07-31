6683fdaa3055ac53c2a95f5c64853a418e51a0f6
ramigoldvarg <ramigoldvarg7@walla.co.il>
2021-07-31 18:26:45 +0300

    seed works

---
+++ u-market/DAL/DatabaseSeed.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using u_market.Models;
+
+namespace u_market.DAL
+{
+    public static class DatabaseSeed
+    {
+        public static void Seed(MarketContext context)
+        {
+            if (!context.Users.Any())
+            {
+                initUsers(context);
+            }
+            if (!context.Stores.Any())
+            {
+                initStores(context);
+            }
+        }
+
+        private static void initUsers(MarketContext context)
+        {
+            // Clients
+            for (int i = 0; i < 5; i++)
+            {
+                User reader = new User()
+                {
+                    Username = "client" + i.ToString(),
+                    Password = i.ToString(),
+                    UserRole = Role.Client,
+                    FirstName = "my-firstname-is-" + i,
+                    LastName = "my-lastname-is-" + i
+                };
+
+                context.Add(reader);
+            }
+
+            // Admin
+            User admin = new User()
+            {
+                Username = "ramig",
+                Password = "ramig2",
+                UserRole = Role.Admin,
+                FirstName = "Rami",
+                LastName = "Goldvarg"
+            };
+
+            context.Add(admin);
+
+            context.SaveChanges();
+        }
+        private static void initStores(MarketContext context)
+        {
+            Store store = new Store()
+            {
+                Name = "Car Shop",
+                Lat = 32.08544449947704,
+                Lang = 34.81442400085823,
+                OwnerId = "ramig"
+            };
+
+            context.Add(store);
+            context.SaveChanges();
+        }
+    }
+}
--- u-market/Startup.cs
+++ u-market/Startup.cs
@@ -38,6 +38,7 @@ namespace u_market
                 {
                     options.LoginPath = "/users/login";
                 });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
