fa6475db04de00186fcdea70ed3eebf4cee01e36
Uriel Wasyng <urielwasyng@gmail.com>
2021-09-04 13:38:29 +0300

    add video

---
+++ u-market/Controllers/Info/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using u_market.DAL;
+using u_market.Models;
+
+namespace u_market.Controllers.Tags
+{
+    [Authorize(Roles = "Client")]
+    public class InfoController : Controller
+    {
+        public InfoController(MarketContext Ctx)
+        {
+        }
+
+        [Authorize(Roles = "Client")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
